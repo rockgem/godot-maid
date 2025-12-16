6d86f46548d22087462699ed506526897813f3cf
Rock Gementiza <63050183+rockgem@users.noreply.github.com>
2025-12-16 23:44:45 +0800

    Update Attack.gd

--- components/states/Attack.gd
+++ components/states/Attack.gd
@@ -5,6 +5,7 @@ var anim_tree
 var state_machine: StateMachine
 
 var is_done = false
+var is_processed = false
 
 func _ready() -> void:
 	await get_tree().physics_frame
@@ -17,13 +18,16 @@ func _ready() -> void:
 func process_state(delta):
 	if is_done == false:
 		anim_tree.get('parameters/playback').travel('attack')
+		
+		if is_processed == false:
+			var obj = state_machine.root_entity.hitcast.get_collider()
+			if obj:
+				obj.take_damage(1)
+			is_processed = true
 	else:
 		state_machine.change_state_by_name('Idle')
 		is_done = false
-		
-		var obj = state_machine.root_entity.hitcast.get_collider()
-		if obj:
-			obj.take_damage(1)
+		is_processed = false
 
 
 func on_animation_finished(animation_name: String):
