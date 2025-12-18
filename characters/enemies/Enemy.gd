878a2d78b11b8bded7851233217cd2c7de96cb07
Rock Gementiza <63050183+rockgem@users.noreply.github.com>
2025-12-18 10:19:42 +0800

    add more states

--- characters/enemies/Enemy.gd
+++ characters/enemies/Enemy.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
 
+@onready var state_machine: StateMachine = $StateMachine
+
 var move_speed = 80.0
 
 
 func _on_hurtbox_hit() -> void:
 	$HitFX.play("hit")
+	state_machine.change_state_by_name('Hit')
 
 
 func _on_hurtbox_zero() -> void:
-	pass # Replace with function body.
+	state_machine.change_state_by_name('Dead')
---
+++ components/states/Hit.gd
@@ -0,0 +1,9 @@
+extends State
+
+
+func enter_state():
+	pass
+
+
+func process_state(delta):
+	pass
