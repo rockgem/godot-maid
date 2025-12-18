4713ed0721d7089279f17bab36f7f0159601b134
Rock Gementiza <63050183+rockgem@users.noreply.github.com>
2025-12-18 12:50:28 +0800

    fix chase state bug

--- components/states/Aggresive.gd
+++ components/states/Aggresive.gd
@@ -5,5 +5,5 @@ extends State
 
 
 func _physics_process(delta: float) -> void:
-	if state_machine.root_entity.global_position.distance_to(ManagerGame.global_player_ref.global_position) < detection_range:
+	if state_machine.root_entity.global_position.distance_to(ManagerGame.global_player_ref.global_position) < detection_range and state_machine.current_state.name != 'Chase':
 		state_machine.change_state_by_name('Chase')
--- components/states/Chase.gd
+++ components/states/Chase.gd
@@ -3,7 +3,7 @@ extends State
 @export var max_distance = 70.0
 
 func enter_state():
-	state_machine.root_entity.animated_sprite.play('run')
+	pass
 
 
 func process_state(delta):
@@ -13,6 +13,8 @@ func process_state(delta):
 	state_machine.root_entity.nav_agent.target_position = ManagerGame.global_player_ref.global_position
 	
 	if state_machine.root_entity.global_position.distance_to(ManagerGame.global_player_ref.global_position) > 16:
+		state_machine.root_entity.animated_sprite.play('run')
+		
 		var path = state_machine.root_entity.nav_agent.get_next_path_position()
 		var dir = state_machine.root_entity.global_position.direction_to(path)
 		state_machine.root_entity.nav_agent.velocity = dir
@@ -20,6 +22,9 @@ func process_state(delta):
 		state_machine.root_entity.direction = dir
 		
 		state_machine.root_entity.global_position += state_machine.root_entity.safe_vel * state_machine.root_entity.move_speed * delta
+	else:
+		state_machine.root_entity.animated_sprite.play('idle')
+		return
 	
 	if state_machine.root_entity.global_position.distance_to(ManagerGame.global_player_ref.global_position) > max_distance:
 		state_machine.change_state_by_name('EnemyIdle')
